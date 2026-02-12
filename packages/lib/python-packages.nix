6bdc24a6014286280f119a4ce1a17f159de0d9ff
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-13 00:07:37 +0900

    biotite: init at 0.39.0

--- packages/lib/python-packages.nix
+++ packages/lib/python-packages.nix
@@ -2,16 +2,14 @@
 # All Python package recipes. Framework-agnostic.
 # Packages receive torch/jax/etc as parameters without knowing which version.
 {
+  byNamePackage,
   mkNvidiaWheel,
   mkWheelPackage,
 }:
-_py-self: _py-super: {
+py-self: _py-super: {
   # -- Helper builders available via callPackage --
   inherit mkNvidiaWheel mkWheelPackage;
 
   # -- Packages --
-  # Add packages here as they are migrated.
-  # Example:
-  # paper-qa = py-self.callPackage (byNamePackage "paper-qa") { };
-  # alphafold3 = py-self.callPackage (byNamePackage "alphafold3") { };
+  biotite = py-self.callPackage (byNamePackage "biotite") { };
 }
