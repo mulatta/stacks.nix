573f37194aa31ce8198a971fc57b7591234cd065
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-13 01:35:36 +0900

    tf-playwright-stealth: init at 1.2.2

---
+++ packages/by-name/tf/tf-playwright-stealth/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  poetry-core,
+  # dependencies
+  playwright,
+  fake-http-header,
+  urllib3,
+}:
+buildPythonPackage {
+  pname = "tf-playwright-stealth";
+  version = "1.2.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "tinyfish-io";
+    repo = "tf-playwright-stealth";
+    rev = "e8aa6d09c17577deee9be59b4b71d95b0d746e9b";
+    hash = "sha256-UP/cNvsQe1HpfqA75t4cVsiKqysq746S7hrzz9O/If0=";
+  };
+
+  build-system = [ poetry-core ];
+
+  dependencies = [
+    playwright
+    fake-http-header
+    urllib3
+  ];
+
+  pythonImportsCheck = [ "playwright_stealth" ];
+
+  doCheck = false;
+
+  meta = {
+    description = "Makes Playwright stealthy like a ninja";
+    homepage = "https://github.com/tinyfish-io/tf-playwright-stealth";
+    license = lib.licenses.mit;
+  };
+}
