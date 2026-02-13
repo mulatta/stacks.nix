ab3e70a404c6165bd0cbfd8e198520f085e1cb31
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-13 11:39:29 +0900

    kalign2: init at 2.04

--- packages/lib/regular-packages.nix
+++ packages/lib/regular-packages.nix
@@ -7,6 +7,7 @@
 {
   edirect = pkgs.callPackage (byNamePackage "edirect") { };
   foldseek = pkgs.callPackage (byNamePackage "foldseek") { };
+  kalign2 = pkgs.callPackage (byNamePackage "kalign2") { };
   ncbi-dataformat = pkgs.callPackage (byNamePackage "ncbi-dataformat") { };
   ncbi-datasets = pkgs.callPackage (byNamePackage "ncbi-datasets") { };
 }
