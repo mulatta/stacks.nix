5c5153c4a14191eb9126a5eda36367401819fee7
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-13 09:38:33 +0900

    graphein: init at 1.7.8

---
+++ packages/by-name/gr/graphein/package.nix
@@ -0,0 +1,109 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  # dependencies
+  pandas,
+  biopandas,
+  biopython,
+  bioservices,
+  cpdb-protein,
+  cython,
+  deepdiff,
+  loguru,
+  looseversion,
+  matplotlib,
+  multipledispatch,
+  networkx,
+  numpy,
+  plotly,
+  pydantic,
+  rich,
+  rich-click,
+  seaborn,
+  pyyaml,
+  scikit-learn,
+  scipy,
+  tqdm,
+  wget,
+  xarray,
+  jaxtyping,
+  # optional deps
+  einops,
+  nest-asyncio,
+}:
+buildPythonPackage (finalAttrs: {
+  pname = "graphein";
+  version = "1.7.8";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "a-r-j";
+    repo = "graphein";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-eJXKWg/V9UOVgKxQimLPhf8p9b2LOHoA9L4dpj3kxDI=";
+  };
+
+  patches = [ ./fix-atoms-rdkit-guard.patch ];
+
+  build-system = [ setuptools ];
+
+  pythonRelaxDeps = [
+    "numpy"
+    "cpdb-protein"
+  ];
+
+  dependencies = [
+    pandas
+    biopandas
+    biopython
+    bioservices
+    cpdb-protein
+    cython
+    deepdiff
+    loguru
+    looseversion
+    matplotlib
+    multipledispatch
+    networkx
+    numpy
+    plotly
+    pydantic
+    rich
+    rich-click
+    seaborn
+    pyyaml
+    scikit-learn
+    scipy
+    tqdm
+    wget
+    xarray
+    jaxtyping
+  ];
+
+  optional-dependencies = {
+    extras = [
+      einops
+      nest-asyncio
+    ];
+  };
+
+  # Tests require network access and external tools (dssp, pymol)
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "graphein"
+    "graphein.protein"
+    "graphein.molecule"
+    "graphein.rna"
+    "graphein.ppi"
+  ];
+
+  meta = {
+    description = "Protein Graph Library";
+    homepage = "https://github.com/a-r-j/graphein";
+    changelog = "https://github.com/a-r-j/graphein/blob/v${finalAttrs.version}/CHANGELOG.md";
+    license = lib.licenses.mit;
+  };
+})
