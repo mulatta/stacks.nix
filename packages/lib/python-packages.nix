7cd00521abbf886aa57cf00b64548f30332ffa42
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-13 09:38:33 +0900

    graphein: init at 1.7.8

--- packages/lib/python-packages.nix
+++ packages/lib/python-packages.nix
@@ -24,6 +24,7 @@ py-self: _py-super: {
   crawl4ai = py-self.callPackage (byNamePackage "crawl4ai") { };
   easydev = py-self.callPackage (byNamePackage "easydev") { };
   fair-esm = py-self.callPackage (byNamePackage "fair-esm") { };
+  graphein = py-self.callPackage (byNamePackage "graphein") { };
   fake-http-header = py-self.callPackage (byNamePackage "fake-http-header") { };
   patchright = py-self.callPackage (byNamePackage "patchright") { };
   tf-playwright-stealth = py-self.callPackage (byNamePackage "tf-playwright-stealth") { };
