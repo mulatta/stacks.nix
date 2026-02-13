b90acd883def3a63743d7e2a3f82de08564d9cbf
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-13 12:45:35 +0900

    chembl-structure-pipeline: init at 1.2.2

---
+++ packages/by-name/ch/chembl-structure-pipeline/package.nix
@@ -0,0 +1,37 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchPypi,
+  setuptools,
+  rdkit,
+}:
+buildPythonPackage rec {
+  pname = "chembl-structure-pipeline";
+  version = "1.2.2";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "chembl_structure_pipeline";
+    inherit version;
+    hash = "sha256-Yl/8JfhtfW/htFmjgbKlL7HfX+DIu/6cYV6Lf3XDAcg=";
+  };
+
+  build-system = [ setuptools ];
+
+  dependencies = [ rdkit ];
+
+  # nixpkgs rdkit is CMake-built (no Python dist-info metadata),
+  # so pythonRuntimeDepsCheck can't find it via importlib.metadata
+  pythonRemoveDeps = [ "rdkit" ];
+
+  # Tests require ChEMBL database access
+  doCheck = false;
+
+  pythonImportsCheck = [ "chembl_structure_pipeline" ];
+
+  meta = {
+    description = "ChEMBL protocols used to standardise and salt strip molecules";
+    homepage = "https://www.ebi.ac.uk/chembl/";
+    license = lib.licenses.mit;
+  };
+}
